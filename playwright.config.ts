ca27f7a78892b4358bad7d24a3f24ebd4479a5bd
Patryk Żurawik <patryk.zurawik@wp.pl>
2025-02-14 17:26:36 +0100

    GHS-8: improve playwright github action (use baseUrl from deployment)

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,15 +18,15 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.PLAYWRIGHT_TEST_BASE_URL ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.PLAYWRIGHT_TEST_BASE_URL ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.cI ? 'dot' : 'list',
+  reporter: process.env.PLAYWRIGHT_TEST_BASE_URL ? 'dot' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL ?? 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
